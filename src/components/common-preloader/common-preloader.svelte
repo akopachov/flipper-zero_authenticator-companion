bb51b4e59aab69ea26df65cd4594ebc2d0225bb5
alex.kopachov <alex.kopachov@gmail.com>
2023-08-31 17:04:29 +0200

    Few UI tweaks for preloader

--- src/components/common-preloader/common-preloader.svelte
+++ src/components/common-preloader/common-preloader.svelte
@@ -7,7 +7,7 @@
 {#if $GlobalPreloader.visible}
   <div
     out:fade={{ duration: 300 }}
-    class="fixed top-0 left-0 w-screen h-screen z-50 flex items-center justify-center flex-col backdrop-blur-md bg-tertiary-100 bg-opacity-50">
+    class="fixed top-0 left-0 w-screen h-screen z-50 flex items-center justify-center flex-col backdrop-blur-md bg-surface-50 dark:bg-surface-600 !bg-opacity-50">
     <ProgressRadial stroke={140} meter="stroke-primary-500" track="stroke-primary-500/30" />
     <h3 class="h3 text-center mt-3 text-current">{$GlobalPreloader.description}</h3>
   </div>

d7cbfd00026da7c25f1af1e21964dcb85ae5ba66
alex.kopachov <alex.kopachov@gmail.com>
2023-08-31 10:57:17 +0200

    Implemented #13

---
+++ src/lib/url-otpauth-ts/error-type.ts
@@ -0,0 +1,12 @@
+export enum ErrorType {
+  InvalidIssuer = 0,
+  InvalidLabel = 1,
+  InvalidProtocol = 2,
+  MissingAccountName = 3,
+  MissingCounter = 4,
+  MissingIssuer = 5,
+  MissingSecretKey = 6,
+  UnknownOtp = 7,
+  InvalidDigits = 8,
+  UnknownAlgorithm = 9,
+}
